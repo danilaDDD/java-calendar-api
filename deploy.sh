052b7ed4335ba2cca5e1b45e3ea111a6369c3090
danila nikitin <danila.n2015@yandex.ru>
2021-08-29 17:39:20 +0300

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+# !/bin/bash
+source_war=/../../build/libs/calendar-0.0.1-SNAPSHOT-plain.war
+target_war=/../../webapps/calendar-api.war
+
+echo "start deploy"
+./gradlew war
+rm -v $target_war
+cp -v $source_war $target_war
+./../../bin/shutdown.sh
+./../../bin/startup.sh
+echo "finish deploy"
